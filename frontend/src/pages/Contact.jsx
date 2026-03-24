c18e7c1931141e355dcb44e1dc59fcd0aa936c63
Ehmkayel <lamidimorufat0@gmail.com>
2026-03-24 20:51:51 +0100

    feat: enhanced features

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -19,9 +19,16 @@ const Contact = () => {
               </span>
             </summary>
             <ul className="list-disc my-4 pl-10 py-3 bg-[#C6E2E8] rounded-xl">
-              {item.users.map((item, i) => (
+              {item.users.map((user, i) => (
                 <li key={i} className="text-base md:text-[22px] font-medium">
-                  {item}
+                  <a
+                    href={`https://mail.google.com/mail/?view=cm&fs=1&to=${user}&su=EcoPulse Inquiry&body=Hello ${user},`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="transition-colors duration-200 hover:text-[#296083] hover:underline cursor-pointer"
+                  >
+                    {user}
+                  </a>
                 </li>
               ))}
             </ul>
